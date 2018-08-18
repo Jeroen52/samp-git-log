fc2115b91d4f627e8dc5f4676a3efdd9e5420ce2
Jeroen de Neef <jeroen52@gmail.com>
2018-08-18 23:13:30 +0200

    samp-git-log now actually outputs 22 lines, somehow I fucked this up.

--- samp-git-log.c
+++ samp-git-log.c
@@ -21,11 +21,11 @@
 //#define DEBUG
 
 #define MAX_LOG_LINES 22
-#define MAX_LOG_LINE_LENGTH 200
+#define MAX_LOG_LINE_LENGTH 173
 #define MAX_LOG_LENGTH (MAX_LOG_LINES*MAX_LOG_LINE_LENGTH)
 #define MAX_INCLUDE_LENGTH (244+MAX_LOG_LENGTH)
 
-#define GIT_LOG_COMMAND "git log --no-merges --pretty=format:'\\n%<(8,trunc)%aN\\t%ad\\t%<(128,trunc)%s\\' --abbrev-commit HEAD^^^^^^^^^^^^^^..HEAD"
+#define GIT_LOG_COMMAND "git log --no-merges --pretty=format:'\\n%<(8,trunc)%aN\\t%ad\\t%<(128,trunc)%s\\' --abbrev-commit HEAD^^^^^^^^^^^^..HEAD"
 
 #ifdef __unix__
 

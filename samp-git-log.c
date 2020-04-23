06b895546d37a9411c242e508f3b48a66d7f51d0
Jeroen de Neef <jeroen52@gmail.com>
2020-04-23 07:52:33 +0200

    So for some reason that git command now means 23 lines instead of 22 lines, which lead to the compiler getting stuck.

--- samp-git-log.c
+++ samp-git-log.c
@@ -23,7 +23,7 @@
 #define MAX_LOG_LENGTH (MAX_LOG_LINES*MAX_LOG_LINE_LENGTH)
 #define MAX_INCLUDE_LENGTH (262+MAX_LOG_LENGTH)
 
-#define GIT_LOG_COMMAND "git log --no-merges --pretty=format:'\\n%<(8,trunc)%aN\\t%ad\\t%<(128,trunc)%s\\' --abbrev-commit HEAD~11..HEAD"
+#define GIT_LOG_COMMAND "git log --no-merges --pretty=format:'\\n%<(8,trunc)%aN\\t%ad\\t%<(128,trunc)%s\\' --abbrev-commit HEAD~10..HEAD"
 
 #ifdef __unix__
 
